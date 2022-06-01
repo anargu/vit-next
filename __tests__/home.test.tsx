a7c6e490b2b2acf48911d0bfcc9b1e2690299fd6
anargu <anargu@pop-os.localdomain>
2022-05-31 21:43:19 -0500

    init

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,14 @@
+export {};
+
+describe("Home", () => {
+  describe("Search fn is used", () => {
+    it("should retrieve all content when input is empty", () => {
+
+    });
+
+    it("should retrieve filtered content when input has a value", () => {
+
+    });
+  });
+
+})
