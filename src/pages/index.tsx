65a75a1a8296da8bb5d41d7e9eaa3760c1f5ee0d
anargu <anargu@pop-os.localdomain>
2022-06-05 17:23:19 -0500

    updated layout css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,16 @@ export const IndexPage = () => {
   const router = useRouter();
 
   return (
-    <div className="mx-6">
+    <>
       <NavBar/>
 
-      <div className="text-stone-900 leading-tight text-4xl max-w-[80%]">
-        VIT, a curated feed of tons of interesting articles, news, tutorials... things.
-      </div>
+      <div className="grid grid-rows-[360px_min-content] mx-6 min-h-[calc(100vh-72px)]">
+        <div className="text-stone-900 leading-tight text-4xl max-w-[80%]">
+          VIT, a curated feed of tons of interesting articles, news, tutorials... things.
+        </div>
 
-      <SearchBar onNewSearch={() => router.push("/feed")} />
-    </div>
+        <SearchBar onNewSearch={() => router.push("/feed")} />
+      </div>
+    </>
   );
 };
