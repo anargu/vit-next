35e7a554821bbcb1a047ab0c17bd62a2e1cc3f69
anargu <anargu@pop-os.localdomain>
2022-06-05 01:29:05 -0500

    moved and udpated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
+import { useRouter } from 'next/router';
 import React from 'react';
-import { MeiliSearchBar } from '../components/MeiliSearch';
+import NavBar from '../components/NavBar';
+import { SearchBar } from '../components/SearchBar';
 
 export const IndexPage = () => {
+  const router = useRouter();
+
   return (
     <div className="mx-6">
+      <NavBar/>
+
       <div className="text-stone-900 leading-tight text-4xl max-w-[80%]">
         VIT, a curated feed of tons of interesting articles, news, tutorials... things.
       </div>
 
-      <MeiliSearchBar />
+      <SearchBar onNewSearch={() => router.push("/feed")} />
     </div>
   );
 };
