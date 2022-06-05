b89550d8db3c3d28e8c9527ccfbf08523c78b4d3
anargu <anargu@pop-os.localdomain>
2022-06-05 01:29:59 -0500

    comment unused comopnent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 // import { SearchBar } from '@/src/components/SearchBar'
-import { MeiliSearchBar } from '@/src/components/MeiliSearch'
+// import { MeiliSearchBar } from '@/src/components/MeiliSearch'
 import { IndexPage } from '@/src/pages'
 
 export default function Home() {
