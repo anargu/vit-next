91d77bd14415645495e23bedf6337cca9e013fc5
anargu <anargu@pop-os.localdomain>
2022-06-05 19:24:55 -0500

    updated tests

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -3,7 +3,7 @@ import { InMemoryRepository } from "../core/InMemoryRepository";
 import { executeSearch } from "../components/SearchBar";
 import { IndexPage } from "../pages/index";
 
-jest.mock('next/link', () => ({ children } : any) => children);
+const IndexTitle = `VIT, a curated feed of tons of interesting articles, news, tutorials... things.`;
 
 describe("Home", () => {
 
@@ -11,7 +11,7 @@ describe("Home", () => {
     it("should have a title", () => {
       const { getByText } = render(<IndexPage/>);
 
-      const matchedEl = getByText(`VIT, a curated feed of tons of interesting articles, news, tutorials... things.`);
+      const matchedEl = getByText(IndexTitle);
 
       expect(matchedEl).toBeTruthy();
     });
@@ -23,6 +23,28 @@ describe("Home", () => {
 
       expect(matchedEl).toBeTruthy();
     });
+
+    it("appears two cards of results below the search bar", () => {
+      const { container } = render(<IndexPage/>);
+
+      // container
+    });
+
+    it("shows more results when scrolled", () => {
+
+    });
+
+    // it("hides title when scrolls down", () => {
+    //   const { container, getByText } = render(<IndexPage/>);
+    //
+    //   expect(container.firstElementChild).toBeInTheDocument();
+    //
+    //   fireEvent.scroll(container.firstElementChild!, { target: { scrollY: 100 } });
+    //
+    //   const titleEl = getByText(IndexTitle);
+    //   expect(titleEl).toHaveClass("hidden");
+    // });
+
   });
 
   // describe("Search fn is used", () => {
