b9a3df8427dc18ec0e2cf65d694a624fd8b73e10
anargu <anargu@pop-os.localdomain>
2022-06-22 07:16:57 -0500

    refactor third party component tests

---
+++ src/components/MeiliSearch.mocks.tsx
@@ -0,0 +1,17 @@
+export const mockMeiliSearchComponent = ({
+  MeiliSearchBar: <div>
+      <form>
+        <input type="search" />
+        <button>o</button>
+        <button>x</button>
+      </form>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </div>,
+});
+
