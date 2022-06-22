23035ed71ad0db4ece1a235b56d72b9007b22f97
anargu <anargu@pop-os.localdomain>
2022-06-22 07:16:28 -0500

    updated snapshot

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,7 +1,15 @@
 import { render } from '@testing-library/react'
 import Home from '@/pages/index'
+import { mockMeiliSearchComponent } from '@/src/components/MeiliSearch.mocks';
+
+jest.mock("../src/components/MeiliSearch", () => ({
+  __esModule: true,
+  MeiliSearchBar: () => {
+    return mockMeiliSearchComponent.MeiliSearchBar;
+  },
+}));
 
 it('renders homepage unchanged', () => {
-  const { container } = render(<Home />)
-  // expect(container).toMatchSnapshot()
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
 })
