45aca54f952b3ccb6a78e0560d652586fb570ad6
anargu <anargu@pop-os.localdomain>
2022-07-31 19:49:50 -0500

    fix IDE not working properly

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+      "cypress/integration/**/*.spec.{js,jsx,ts,tsx}",
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
