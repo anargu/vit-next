53f46846f9ec134d64d50ce505cabeee18ec46d9
anargu <anargu@pop-os.localdomain>
2022-07-31 19:51:53 -0500

    updated UI property

--- src/components/ResourceCard/BackgroundImage.tsx
+++ src/components/ResourceCard/BackgroundImage.tsx
@@ -8,7 +8,7 @@ export const BackgroundImage = (props : BackgroundImageProps) => (
   <div
     role="img"
     alt={props.alt}
-    className={`w-full h-full bg-contain bg-no-repeat`}
+    className={`w-full h-full bg-cover bg-no-repeat`}
     style={{ backgroundImage: `url(${props.src})`}}
   />
 );
