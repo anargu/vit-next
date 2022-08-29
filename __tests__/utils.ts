9987106ca3ac3f2f4fd5d9cb38da6ed39dd191a1
anargu <anargu@pop-os.localdomain>
2022-08-29 08:55:45 -0500

    added utils

---
+++ __tests__/utils.ts
@@ -0,0 +1,15 @@
+import { VITResource } from "@/src/core/entities";
+import { faker } from "@faker-js/faker";
+
+faker.seed(1);
+
+export const mockedResource = () : VITResource => ({
+  id: faker.datatype.uuid(),
+  og_image: "https://source.unsplash.com/random/50x50",
+  keyphrase: faker.lorem.sentence(5),
+  date_created: faker.datatype.datetime().toISOString(),
+  og_title: faker.lorem.sentence(4),
+  og_description: faker.lorem.sentence(12),
+  url: faker.internet.url(),
+  url_title: faker.lorem.sentence(4),
+});
