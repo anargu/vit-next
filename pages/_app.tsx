cbe77e95a3872371b8acb1b22b4d700566fec032
anargu <anargu@pop-os.localdomain>
2022-08-29 23:21:38 -0500

    added page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { WithNotificationsProvider } from '@/src/components/Notification/Notification'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import PageTransition from '../src/components/Transitions/PageTransition';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WithNotificationsProvider>
-      <Component {...pageProps} />
+      <PageTransition>
+        <Component {...pageProps} />
+      </PageTransition>
     </WithNotificationsProvider>
   );
 }
---
+++ src/components/Transitions/PageTransition.tsx
@@ -0,0 +1,46 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+const variants = {
+  out: {
+    opacity: 0,
+    y: 40,
+    transition: {
+      duration: 0.75
+    }
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.75,
+      delay: 0.5
+    }
+  }
+};
+
+const PageTransition = ({ children } : any) => {
+  const { asPath } = useRouter();
+
+  return (
+    <div className="">
+      <AnimatePresence
+	      initial={false}
+	      exitBeforeEnter
+	    >
+	      <motion.div
+          key={asPath}
+          variants={variants}
+          animate="in"
+          initial="out"
+          exit="out"
+        >
+	        {children}
+	      </motion.div>
+	    </AnimatePresence>
+    </div>
+  );
+};
+
+export default PageTransition;
+
