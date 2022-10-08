099c83d629ac39b3895f5538645297d7b28a188e
anargu <anargu@pop-os.localdomain>
2022-10-08 01:27:05 -0500

    added feature save url

--- src/pages/saved.test.tsx
+++ src/pages/saved.test.tsx
@@ -1,4 +1,6 @@
-import { render } from "@testing-library/react";
+import { userEvent } from "@storybook/testing-library";
+import { findByPlaceholderText, render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { mockedResource } from "../../__tests__/utils";
 import { SAVED_LINK_KEY } from "../components/ResourceCard/ResourceCard";
 import { SavedPage } from "./saved";
@@ -36,5 +38,56 @@ describe("Saved", () => {
     await findByText(oldPostTwo.og_title!);
   });
 
+  it("should delete an already saved post if is clicked on save button", async () => {
+    const oldPostOne = mockedResource();
+
+    localStorage.setItem(SAVED_LINK_KEY, JSON.stringify([oldPostOne]));
+
+    const { findByTitle, queryByText } = render(<SavedPage />);
+
+    const saveButton = await findByTitle("Save Button");
+
+    act(() => {
+      userEvent.click(saveButton);
+    });
+
+    const ogTitleEl = queryByText(oldPostOne.og_title ?? "");
+
+    expect(ogTitleEl).toBeNull();
+  });
+
+  describe("Save custom URLs", () => {
+    it("opens a dialog to insert URL", async () => {
+      const { findByText, findByTitle } = render(<SavedPage />);
+
+      userEvent.click(await findByTitle("Save a Link"));
+      
+      expect(findByText("Insert a URL you want to save"));
+    });
+
+    it("saves locally the provided url on text input and pressed Save", async () => {
+      const PLACEHOLDER = "Paste or type url";
+      const { findByText, findByTitle, findByPlaceholderText, queryByText } = render(<SavedPage />);
+
+      userEvent.click(await findByTitle("Save a Link"));
+      const inputEl = await findByPlaceholderText(PLACEHOLDER);
+      expect(inputEl).not.toBeFalsy();
+
+      const URL_TO_SAVE = "https://www.google.com";
+      userEvent.paste(inputEl, URL_TO_SAVE);
+
+      await act(async () => {
+        const buttonEl = await findByText("Save");
+        userEvent.click(buttonEl);
+      });
+
+      const value = localStorage.getItem(SAVED_LINK_KEY);
+      expect(value).not.toBeFalsy();
+      expect(value).toMatch(URL_TO_SAVE);
+
+      const text = queryByText(PLACEHOLDER);
+      expect(text).toBeNull();
+    });
+  });
 });
 
--- src/pages/saved.tsx
+++ src/pages/saved.tsx
@@ -1,22 +1,80 @@
-import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { BottomSheetWrapper } from '../components/BottomSheetWrapper';
 import NavBar from '../components/NavBar';
 import { ResourceCard } from '../components/ResourceCard/ResourceCard';
 import { useSavedResources } from '../hooks/useSavedResources';
 
 export const SavedPage = () => {
 
-  const { savedResources } = useSavedResources();
+  const { savedResources, saveResource } = useSavedResources();
+
+  const [showSaveUrlSheet, setShowSaveUrlSheet] = useState(false);
+
+  type SubmitFormTypes = {
+    inputLink: string
+  };
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<SubmitFormTypes>();
+
+  const onSubmitCutomLink : SubmitHandler<SubmitFormTypes> = (data) => {
+    saveResource(data.inputLink);
+
+    setShowSaveUrlSheet(false);
+    reset();
+  };
 
   const PageWrapper = ({ children } : any) => (
     <>
       <NavBar/>
+
+      <div className="py-4 text-center text-gray-400 text-sm leading-8">
+        Wanna save your links? <span
+          onClick={(e) => {
+            // opens sheet 
+            e.stopPropagation();
+
+            setShowSaveUrlSheet(true);
+          }}
+          className="rounded px-2 underline cursor-pointer py-1 text-yellow-400 text-white" title="Save a Link">Save yours here.</span>
+      </div>
+
+      <BottomSheetWrapper show={showSaveUrlSheet} onCloseSheet={() => {setShowSaveUrlSheet(false);}}>
+        <div className="pb-8 pt-4 px-4">
+          <form onSubmit={handleSubmit(onSubmitCutomLink)}>
+            <div className="mb-4">
+              <label htmlFor="custom-url-input">Insert a URL you want to save</label>
+              <input
+                id="custom-url-input"
+                type="text"
+                autoFocus
+                placeholder="Paste or type url"
+                className="bg-slate-200 rounded outline-none py-2 px-2 w-full"
+                {...register("inputLink", { required: true })}
+              />
+              { errors.inputLink && <span className="text-red-500">Error </span> }
+            </div>
+            <div className="text-right">
+              <input type="submit" value="Save" className="inline-block bg-yellow-400 px-4 py-2 rounded" />
+            </div>
+          </form>
+        </div>
+      </BottomSheetWrapper>
 
       <div className="min-h-[calc(100vh-72px)]">
         {children}
       </div>
     </>
   );
+
+  const SavedPostsList = useMemo(() => {
+    if (!savedResources) return null;
+
+    return savedResources.map((resource, index) => (
+      <div key={`resource-${index}`}>
+        <ResourceCard hit={resource} onSaveResource={saveResource}></ResourceCard>
+      </div>
+    ))
+  }, [savedResources]);
 
   // savedResources is null, it is loading
   if (savedResources === null) return (
@@ -33,12 +91,8 @@ export const SavedPage = () => {
   );
 
   return (
-    <PageWrapper>
-      {savedResources.map((resource, index) => (
-        <div key={`resource-${index}`}>
-          <ResourceCard hit={resource}></ResourceCard>
-        </div>
-      ))}
+    <PageWrapper key={savedResources}>
+      {SavedPostsList}
     </PageWrapper>
   );
 };
