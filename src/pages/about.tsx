0e6216b527f9ac8abd0766170265b6dc4d493bb0
anargu <anargu@pop-os.localdomain>
2022-10-19 23:48:20 -0500

    added contributions

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,12 +9,18 @@ export const AboutPage = () => {
 
       <div className="grid gap-2 min-h-[calc(100vh-72px)]">
         <h1 className="text-center py-2">About VIT</h1>
-        <div className="leading-tight text-lg max-w-[80%] mx-6">
+        <div className="leading-tight text-lg max-w-[80%] mx-6 mb-2">
           VIT is formed by a small group of Peruvian developers. Our goal (for now) is to
           share useful tools in any sub-field related to Computer Science, Art, UI/UX, XR, and so on.
           We surf on the internet (as probably you do) and collect urls, resources, blogs that we consider <span className="font-bold">interesting</span>.
           Then we share in our small Telegram group those <span className="font-bold">Very Interesting Things (VIT).</span>
         </div>
+
+        <div className="leading-tight text-lg max-w-[80%] mx-6 mb-2">
+          Special thanks to <a href="https://www.instagram.com/m4xlr/">Max</a>. His work on the UI design was vital and inspiring to implement the frontend part
+        </div>
+
+
         <div className="leading-tight float-right text-right max-w-[80%] mx-6">
           <h1 className="mx-0 font-bold text-xl">Website's Author</h1>
           <p className="text-lg">Anthony Aróstegui.<br/>(but you can call me anargu).</p>
