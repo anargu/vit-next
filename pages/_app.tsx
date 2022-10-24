3896397909a196dab27d65319168a526d00ba1fb
anargu <anargu@pop-os.localdomain>
2022-10-24 01:05:50 -0500

    updated NavBar w/ Animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import NavBar from '@/src/components/NavBar';
 import { WithNotificationsProvider } from '@/src/components/Notification/Notification'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 import PageTransition from '../src/components/Transitions/PageTransition';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function VITApp({ Component, pageProps }: AppProps) {
   return (
     <WithNotificationsProvider>
+      <NavBar/>
+
       <PageTransition>
         <Component {...pageProps} />
       </PageTransition>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,9 +2,33 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { NavBarItemMenu } from "./NavBarMenu";
 import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const FEED_LABEL = "Feed";
 export const SAVED_LABEL = "Saved";
+
+const variants = {
+  out: {
+    backgroundColor: "#FFF",
+    opacity: 0,
+    transition: {
+      duration: 0.75
+    }
+  },
+  in: {
+    backgroundColor: "#FFF",
+    transition: {
+      duration: 0.75,
+    }
+  },
+  aboutIn: {
+    backgroundColor: "#d1fae5",
+    transition: {
+      duration: 0.75,
+      delay: 1.5
+    }
+  }
+};
 
 interface NavBarItemProps {
   active?: boolean,
@@ -16,16 +40,13 @@ const NavBarItem = ({ active, children, ...props  } : NavBarItemProps) => (
   <div className={`${active ? "text-base text-black-400 underline" : "text-base text-gray-400"} cursor-pointer text-center hover:text-gray-800`} {...props}>{children}</div>
 );
 
-interface NavBarProps {
-}
-
 interface LeftItem {
   label: string,
   active: boolean,
   path: string,
 }
 
-export const NavBar = (props : NavBarProps) => {
+export const NavBar = () => {
   const router = useRouter();
   const { pathname } = router;
 
@@ -40,29 +61,40 @@ export const NavBar = (props : NavBarProps) => {
   };
 
   return (
-    <div className="grid grid-cols-[20px_48px_48px_auto_70px] lg:grid-cols-[24px_96px_96px_auto_70px] items-center h-[72px] mx-4">
-      <Image src="/assets/v_icon_512px.png" width="24px" height="24px" sizes="(min-width: 1024px) 24px, 20px"/>
-      {leftItems.map((item) =>
-        <NavBarItem
-          key={item.label}
-          active={item.path === pathname}
-          onClick={() => onSelectItem(item)}
-        >{item.label}</NavBarItem>)}
+    <AnimatePresence
+      initial={false}
+      mode="wait"
+    >
+      <motion.div
+        variants={variants}
+        animate={router.pathname === "/about" ? "aboutIn" : "in"}
+        initial="out"
+        exit="out"
+        className="bg-emerald-100 grid grid-cols-[20px_48px_48px_auto_70px] lg:grid-cols-[24px_96px_96px_auto_70px] items-center h-[72px] px-4"
+      >
+        <Image src="/assets/v_icon_512px.png" width="24px" height="24px" sizes="(min-width: 1024px) 24px, 20px"/>
+        {leftItems.map((item) =>
+          <NavBarItem
+            key={item.label}
+            active={item.path === pathname}
+            onClick={() => onSelectItem(item)}
+          >{item.label}</NavBarItem>)}
 
-      <div className="inline-block w-full"/>
+        <div className="inline-block w-full"/>
 
-      <div className="inline-grid grid-cols-1 gap-6">
-        {/*
-          TODO: Switch layout feature
-          <Image src="/assets/mosaic.svg" width="28px" height="28px" />
-        */}
-        <NavBarItemMenu right menu={[
-          { label: "About us", url: "/about" }
-        ]}>
-          <Image src="/assets/more-horizontal.svg" width="28px" height="28px" />
-        </NavBarItemMenu>
-      </div>
-    </div>
+        <div className="inline-grid grid-cols-1 gap-6">
+          {/*
+            TODO: Switch layout feature
+            <Image src="/assets/mosaic.svg" width="28px" height="28px" />
+          */}
+          <NavBarItemMenu right menu={[
+            { label: "About us", url: "/about" }
+          ]}>
+            <Image src="/assets/more-horizontal.svg" width="28px" height="28px" />
+          </NavBarItemMenu>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
