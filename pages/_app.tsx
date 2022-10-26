d8329d7f4f08f5465ac3a130e6c736c2ef1fe76a
anargu <anargu@pop-os.localdomain>
2022-10-25 23:56:22 -0500

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,62 @@
+declare global {
+    interface Window { gtag: any; }
+}
+
 import NavBar from '@/src/components/NavBar';
 import { WithNotificationsProvider } from '@/src/components/Notification/Notification'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import Script from 'next/script'
+import { useEffect } from 'react';
 import PageTransition from '../src/components/Transitions/PageTransition';
 
 export default function VITApp({ Component, pageProps }: AppProps) {
-  return (
-    <WithNotificationsProvider>
-      <NavBar/>
 
-      <PageTransition>
-        <Component {...pageProps} />
-      </PageTransition>
-    </WithNotificationsProvider>
+  const router = useRouter();
+
+  useEffect(() => {
+  	const handleRouteChange = (url : string) => {
+  		window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+  			page_path: url,
+  		});
+  	};
+
+  	router.events.on("routeChangeComplete", handleRouteChange);
+
+  	return () => {
+  	router.events.off("routeChangeComplete", handleRouteChange);
+  	};
+  }, [router.events]);
+
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <WithNotificationsProvider>
+        <NavBar/>
+
+        <PageTransition>
+          <Component {...pageProps} />
+        </PageTransition>
+      </WithNotificationsProvider>
+    </>
   );
 }
