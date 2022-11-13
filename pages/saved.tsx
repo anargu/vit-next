30c1c9d61a3eeffc8ba8bcf58706d996740cf489
anargu <anargu@pop-os.localdomain>
2022-11-13 15:40:56 -0500

    refactor title head

--- pages/saved.tsx
+++ pages/saved.tsx
@@ -6,7 +6,7 @@ export default function Saved() {
   return (
     <div>
       <Head>
-        <title>Saved Posts - {SEO.commonTitlePhrase}</title>
+        <title>Saved Posts - {SEO.titleVITPhrase}</title>
       </Head>
       
       <SavedPage/>
