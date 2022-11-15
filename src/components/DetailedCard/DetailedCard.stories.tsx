715373922ed99efbff390e8c166d4129268517e0
anargu <anargu@pop-os.localdomain>
2022-11-14 20:10:52 -0500

    fix type on storybook

--- src/components/DetailedCard/DetailedCard.stories.tsx
+++ src/components/DetailedCard/DetailedCard.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { DetailedCard, DetailedCardProps } from "./DetailedCard";
 
 import { faker } from "@faker-js/faker";
-import { VITResource } from '@/src/core/entities';
+import { Resource, VITResource } from '@/src/core/entities';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'UI/DetailedCard',
@@ -19,7 +19,7 @@ const Template: ComponentStory<typeof DetailedCard> = (args : DetailedCardProps)
   <div className="bg-gray-100"><DetailedCard {...args} /></div>
 );
 
-const mockHitData = () : VITResource => ({
+const mockVITResourceData = () : VITResource => ({
   id: faker.datatype.uuid(),
   og_image: "https://source.unsplash.com/random/50x50",
   keyphrase: "man holding a beer",
@@ -33,6 +33,6 @@ const mockHitData = () : VITResource => ({
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
-  hit: mockHitData(),
+  hit: Resource.fromVITResource(mockVITResourceData()),
 };
 
