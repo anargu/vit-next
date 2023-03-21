0037ef086cc3762ad2a6ec225e8943922d4e9825
anargu <anargu@pop-os.localdomain>
2023-03-21 01:35:27 -0500

    set MetadataResponse interface

---
+++ pages/api/metadata/entities.ts
@@ -0,0 +1,11 @@
+
+export interface MetadataResponse {
+  title?: string,
+  author?: string,
+  date?: string,
+  description?:string,
+  image?: string,
+  logo?: string,
+  publisher?: string,
+  keywords?: string,
+};
