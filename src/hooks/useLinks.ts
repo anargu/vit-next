a5a633072b68707ebd0acdf5fe21de533a390c79
anargu <anargu@pop-os.localdomain>
2023-04-01 23:03:35 -0500

    refactored hooks

--- src/hooks/useLinks.ts
+++ src/hooks/useLinks.ts
@@ -1,45 +1,145 @@
-import { useEffect, useState } from "react";
-import { Resource, User } from "../core/entities";
-import { listenFeedLinks, listenLinksFromUser } from "../services/datasource";
-import { DataTransformer, Link } from "../services/entities";
+import { useEffect, useMemo, useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
-export function useLinks (user : User | null) {
+import { useAuth } from "./useAuth";
+import { DOMAIN_REGEX, SAVED_LINK_KEY } from "../core/constants";
+import { fetchMetadataFromURL } from "../services/metadataService";
+import { JSONResource, LinkPrivacy, Resource } from "../core/entities";
+import { FirestoreStorageStrategy } from "../services/FirestoreStorageStrategy";
+import { ga, ON_CLICK_SAVE_POST_EVENT, SAVE_POST_SUCESSFULLY_EVENT } from "../../lib/ga";
 
-  const [userLinks, setUserLinks] = useState<Resource[] | null>([]);
+const firestoreDataManager = new FirestoreStorageStrategy();
+
+export type UpdatePrivacySettingFn = (resourceId : string, privacy : LinkPrivacy) => Promise<void>;
+export type SaveResourceFn = (resource : string | Resource) => Promise<void>;
+export type DeleteResourceFn = (resourceId : string) => Promise<void>;
+
+export interface useLinksOptions {
+  listenSavedLinks?: boolean;
+  listenFeedLinks?: boolean;
+};
+
+export function useLinks (opts: useLinksOptions = { listenSavedLinks: false, listenFeedLinks: false }) {
+
+  const { authUser, isAuthenticated } = useAuth();
+
+  const [_userLinks, _setUserLinks] = useState<Resource[] | null>([]);
 
   const [feedLinks, setFeedLinks] = useState<Resource[] | null>([]);
 
-  useEffect(() => {
-    if (!user) return;
+  // localLinks are saved links in LocalStorage 
+  const [localLinks, setLocalLinks, { removeItem: deleteLocalLinks }] = useLocalStorageState<JSONResource[]>(SAVED_LINK_KEY, { defaultValue: [] });
 
-    const unSub = listenLinksFromUser(user.id, (linksSnap) => {
-      setUserLinks(linksSnap.docs.map((link) => DataTransformer.fromLinktoResource({
-        ...link.data() as Link,
-        id: link.id,
-      })));
+  const userLinks = useMemo(() => {
+    if (isAuthenticated) return _userLinks;
+
+    return localLinks.map((localLink) => Resource.fromJSONResource(localLink));
+  }, [isAuthenticated, localLinks, _userLinks]);
+
+  // TODO: Implement this
+  /* const initListeningLinksByUserId = (userId: string) => { */
+  /*   const unSub = firestoreDataManager.listenLinksbyUserId(userId, (resources) => { */
+  /*     _setUserLinks(resources); */
+  /*   }); */
+  /**/
+  /*   return unSub; */
+  /* }; */
+
+  useEffect(() => {
+    if (!authUser) return;
+    if (!opts.listenSavedLinks) return;
+
+    const unSub = firestoreDataManager.listenLinksbyUserId(authUser!.id, (resources) => {
+      _setUserLinks(resources);
     });
 
     return () => {
       unSub?.();
     };
-  }, [user]);
+  }, [authUser?.id, opts.listenSavedLinks]);
 
-  const initListeningFeed = () => {
-    const unSub = listenFeedLinks((linksSnap) => {
-      const feedResources_ = linksSnap.docs.map((link) => DataTransformer.fromLinktoResource({
-        ...link.data() as Link,
-        id: link.id,
-      }));
+  useEffect(() => {
+    if (!opts.listenFeedLinks) return;
 
-      setFeedLinks(feedResources_);
+    const unSub = firestoreDataManager.listenFeedLinks((resources) => {
+      setFeedLinks(resources);
     });
 
-    return unSub;
+    return () => {
+      unSub?.();
+    };
+  }, [opts.listenFeedLinks]);
+
+  const insertLink = async (url : string) => {
+    try {
+      ga.event({ action: ON_CLICK_SAVE_POST_EVENT, params: {} });
+
+      // Fetch Metadata of URL string
+      let resourceMetadata = await fetchMetadataFromURL(url);
+
+      // Save resource and its metadata
+      const insertPromise = isAuthenticated
+        ? firestoreDataManager.insert(resourceMetadata, authUser)
+        : setLocalLinks([...localLinks, resourceMetadata.toJSON()]);
+
+      await insertPromise;
+
+      // Track analytics event
+      const domain = url.match(DOMAIN_REGEX)?.[0];
+
+      ga.event({
+        action: SAVE_POST_SUCESSFULLY_EVENT,
+        params: { domain: domain }
+      });
+    } catch (error : any) {
+      console.error(error);
+
+      throw error;
+    }
+  };
+
+  const updateLinkPrivacy = async (resourceId: string, privacy: LinkPrivacy) => {
+    try {
+      if (!isAuthenticated) {
+        throw Error("Operation cannot be done if user is not logged in.");
+      }
+
+      // User is logged in then update to DB.
+      await firestoreDataManager.updateLinkPrivacy(resourceId, privacy);
+
+    } catch (error : any) {
+      console.error(error);
+
+      throw error;
+    }
+  };
+
+  const deleteLink = async (resourceId: string) => {
+    try {
+      if (!isAuthenticated) {
+        setLocalLinks(localLinks.filter((_link) => _link.id !== resourceId));
+        return;
+      }
+
+      await firestoreDataManager.delete(resourceId);
+    } catch (error : any) {
+      console.error(error);
+
+      throw error;
+    }
   };
 
   return {
+    localLinks,
+    deleteLocalLinks,
+
     userLinks,
-    initListeningFeed,
     feedLinks,
+
+    insertLink,
+    deleteLink,
+    updateLinkPrivacy,
+
+    bulkInsertLinks: firestoreDataManager.bulkInsert,
   };
 }
--- src/hooks/useResource.ts
+++ src/hooks/useResource.ts
@@ -2,7 +2,6 @@ import { showDefaultNotification } from "../components/Notification/Notification
 import { Resource, VITResource } from "../core/entities";
 
 export const useResource = () => {
-
   const share = (resource : Resource | VITResource) => {
     navigator.clipboard.writeText(resource?.url || "");
 
